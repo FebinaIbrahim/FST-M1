0f4fde9bacefe9a8ec44b183792b16dad5a5e799
FebinaIbrahim <Febina.Ibrahim@ibm.com>
2025-12-04 11:10:29 +0530

    Add files via upload

---
+++ Python/Activities/activity3.py
@@ -0,0 +1,29 @@
+# Get the users names
+user1 = input("What is Player 1's name? ")
+user2 = input("What is Player 2's name? ")
+
+# Get the users choices
+user1_response = input(user1 + ",What do you Choose among [rock, paper or scissors]?").lower()
+user2_response = input(user2 + ",What do you Choose among [rock, paper or scissors]?").lower()
+
+# Run the game logic to determine who wins
+if user1_response == user2_response:
+    print("It's a tie!")
+elif user1_response == 'rock':
+    if user2_response == 'scissors':
+        print(user1+" wins!")
+    else:
+        print(user2+" wins!")
+elif user1_response == 'scissors':
+    if user2_response == 'paper':
+        print(user1+" wins!")
+    else:
+        print(user2+" wins!")
+elif user1_response == 'paper':
+    if user2_response == 'rock':
+        print(user1+" wins!")
+    else:
+        print(user2+" wins!")
+    
+else:
+    print("Invalid input! You have not entered rock, paper or scissors, try again.")
