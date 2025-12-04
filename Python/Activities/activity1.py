ded814c8c8c1370fa36754ec7c09eb52021c5ede
FebinaIbrahim <Febina.Ibrahim@ibm.com>
2025-12-04 10:40:06 +0530

    Add files via upload

---
+++ Python/Activities/activity1.py
@@ -0,0 +1,12 @@
+name = input("Enter your name:")
+age  = int(input("Enter you age:"))
+
+#calculating year when you turn 100 years
+year = ( (2025-age) + 100 )
+
+print(f"Your name is:{name}")
+print(f"Your age is:{age}")
+print(f"Year when {name} turn 100 years is:{year}")
+
+ 
+
